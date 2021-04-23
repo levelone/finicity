52a3475af4949fc0b1ff189b58b50069cb1255ee
Maynard Landrito <landritom@yahoo.com>
2021-04-23 12:11:17 +0800

    add generate_finicity_fix_connect_link

--- lib/finicity/v2.rb
+++ lib/finicity/v2.rb
@@ -4,6 +4,7 @@ require 'finicity/v2/request/get_all_institutions'
 require 'finicity/v2/request/get_institution_by_id'
 require 'finicity/v2/request/add_customer'
 require 'finicity/v2/request/generate_finicity_connect_link'
+require 'finicity/v2/request/generate_finicity_fix_connect_link'
 require 'finicity/v2/response/partner_authentication'
 
 module Finicity
